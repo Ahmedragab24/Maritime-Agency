83df3a3073d90326b2006ad8b7aec513967d43e9
Ahmed Ragab <89437174+Ahmedragab24@users.noreply.github.com>
2023-06-28 19:49:11 +0200

    Edit

--- assest/Js/main.js
+++ assest/Js/main.js
@@ -33,12 +33,12 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-// function scrollHeader(){
-//     const header = document.getElementById('header')
-//     // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-//     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
-// }
-// window.addEventListener('scroll', scrollHeader)
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SWIPER DISCOVER ====================*/
 let swiperPopular = new Swiper(".popular__container", {
